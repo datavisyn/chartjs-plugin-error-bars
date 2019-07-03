0c6f7b4fd2a568e461bafe450dffe5ee3304db81
sluger <stefan.luger@datavisyn.io>
2019-07-03 10:35:50 +0200

    add mjs to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,34 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
-export default {
-  external: ['chart.js'],
+export default [{
   output: {
     file: 'build/Plugin.Errorbars.js',
+    name: 'PluginErrorbars',
     format: 'umd',
     globals: {
       'chart.js': 'Chart'
-    },
-    name: 'PluginErrorbars'
+    }
   },
+  external: ['chart.js'],
   plugins: [
     resolve(),
     commonjs(),
     babel()
   ]
-};
+}, {
+  output: {
+    file: 'build/Plugin.Errorbars.mjs',
+    name: 'PluginErrorbars',
+    format: 'esm',
+    globals: {
+      'chart.js': 'Chart'
+    }
+  },
+  external: ['chart.js'],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel()
+  ]
+}];
